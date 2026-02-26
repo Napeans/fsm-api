d141205961ad31fc55bc6304a050d5e5b0d62ee0
aruls <aruls@DESKTOP-E8P2GMP>
2026-02-26 10:54:00 +0530

    hjkhk

---
+++ fsm-api/Models/JobModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fsm_api.Models
+{
+    public class JobsModel
+    {
+        public int JobId { get; set; }
+        public string JobNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string MobileNo { get; set; }
+        public string ServiceName { get; set; }
+        public DateTime? ScheduledOn { get; set; }
+        public string Address { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string JobStatus { get; set; }
+    }
+}
---
+++ fsm-api/Repository/JobRepository.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using fsm_api.Common;
+using fsm_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace fsm_api.Repository
+{
+ 
+    public class JobRepository
+    {
+        private readonly DapperDataService _dataService;
+
+        public JobRepository()
+        {
+            _dataService = new DapperDataService();
+        }
+        public async Task<List<JobsModel>> GetMyJobs()
+        {
+            var parameters = new DynamicParameters();
+
+            string query = @"SELECT JobId,[JobNumber],CustomerName,MobileNo,d.ServiceName,a.ScheduledOn,
+e.AddressLine1+','+e.Area+','+e.City+','+e.[State]+','+e.Pincode as [Address],e.[Latitude],e.[Longitude],a.JobStatus
+
+  FROM [Jobs] as a inner join [Customers] as b on a.[CustomerId]=b.[CustomerId]
+  inner join [Leads] as c on c.LeadId=a.LeadId
+  inner join [ServiceTypes] as d on d.ServiceTypeId=c.ServiceTypeId
+  inner join CustomerAddresses as e on e.CustomerAddressId=c.CustomerAddressId";
+
+
+            var list = await _dataService.GetAllAsync<JobsModel>(query,parameters);
+
+            return list.ToList();
+        }
+
+        //public async Task<(List<Mst_Scrap_Type>, List<ProductDetailsModel>)> GetProductDetails(int CityId)
+        //{
+        //    var parameters = new DynamicParameters();
+        //    parameters.Add("@CityID", CityId);
+        //    using (var multi = await _dataService.QueryMultipleAsync(
+        //        "GETProductList",
+        //        parameters,
+        //        CommandType.StoredProcedure))
+        //    {
+        //        var scrapTypes = (await multi.ReadAsync<Mst_Scrap_Type>()).AsList();
+        //        var scrapCategories = (await multi.ReadAsync<ProductDetailsModel>()).AsList();
+        //        return (scrapTypes, scrapCategories);
+        //    }
+        //}
+    }
+}

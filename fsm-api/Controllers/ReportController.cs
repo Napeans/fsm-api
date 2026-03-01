88cc502e2335e37bb3349e32f854815fb438edc2
aruls <aruls@DESKTOP-E8P2GMP>
2026-03-01 19:39:53 +0530

    tytghj

--- fsm-api/Controllers/ReportController.cs
+++ fsm-api/Controllers/ReportController.cs
@@ -14,7 +14,7 @@ using System.Web.Http;
 
 namespace fsm_api.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [RoutePrefix("api/report")]
     public class ReportController : ApiController
     {
